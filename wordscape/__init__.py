be6bc9313a54828254970e5b09b8a785012eaee3
Austin Hartzheim <inbox@austinhartzheim.me>
2016-07-19 02:50:45 +0000

    Implemented basic changeset summarization.

---
+++ wordscape/__init__.py
@@ -0,0 +1,104 @@
+class Changeset():
+    TEMPLATE = ('# Changeset #{changeset_id}\n{summary}\n\n'
+                '## Detailed Changes\n{detailed_changes}')
+    MSG_STANDARD = ('This changeset included {additions} additions, '
+                    '{modifications} modifications, and {deletions} deletions, '
+                    'making {total_changes} changes in total.')
+
+    def __init__(self, changeset_id, changeset_data):
+        self.cs_id = changeset_id
+        self.cs_data = changeset_data
+
+    def describe(self):
+        return self.TEMPLATE.format(
+            changeset_id=self.cs_id,
+            summary=self.summarize(),
+            detailed_changes=self.describe_changes()
+        )
+
+    def describe_changes(self):
+        return 'Not implemented yet.'
+
+    def summarize(self):
+        additions, modifications, deletions = 0, 0, 0
+        nodes_added, nodes_modified, nodes_deleted = 0, 0, 0
+        ways_added, ways_modified, ways_deleted = 0, 0, 0
+        relations_added, relations_modified, relations_deleted = 0, 0, 0
+
+        nodes_touched, ways_touched, relations_touched = set(), set(), set()
+
+        for change in self.cs_data:
+            # Collect aggregate "touch" statistics
+            if change['type'] == 'node':
+                nodes_touched.add(change['data']['id'])
+            elif change['type'] == 'way':
+                ways_touched.add(change['data']['id'])
+            elif change['type'] == 'relation':
+                relations_touched.add(change['data']['id'])
+
+            # Collect per-type statistics
+            if change['action'] == 'create':
+                additions += 1
+                if change['type'] == 'node':
+                    nodes_added += 1
+                elif change['type'] == 'way':
+                    nodes_modified += 1
+                elif change['type'] == 'relation':
+                    nodes_deleted += 1
+            elif change['action'] == 'modify':
+                modifications += 1
+                if change['type'] == 'node':
+                    ways_added += 1
+                elif change['type'] == 'way':
+                    ways_modified += 1
+                elif change['type'] == 'relation':
+                    ways_deleted += 1
+            elif change['action'] == 'delete':
+                deletions += 1
+                if change['type'] == 'node':
+                    relations_added += 1
+                elif change['type'] == 'way':
+                    relations_modified += 1
+                elif change['type'] == 'relation':
+                    relations_deleted += 1
+
+        total_changes = sum((additions, modifications, deletions))
+
+        msg_data = {
+            'additions': additions,
+            'modifications': modifications,
+            'deletions': deletions,
+            'total_changes': total_changes,
+
+            'nodes_added': nodes_added,
+            'nodes_modified': nodes_modified,
+            'nodes_deleted': nodes_deleted,
+            'ways_added': ways_added,
+            'ways_modified': ways_modified,
+            'ways_deleted': ways_deleted,
+            'relations_added': relations_added,
+            'relations_modified': relations_modified,
+            'relations_deleted': relations_deleted,
+
+            'nodes_touched': len(nodes_touched),
+            'ways_touched': len(ways_touched),
+            'relations_touched': len(relations_touched),
+        }
+
+        # Generate message
+        message = []
+        message.append(self.MSG_STANDARD)
+
+        # Temporarily remove: It's unclear what it means for something to be
+        #  the majority of an edit
+#        if len(nodes_touched) / total_changes > 0.5:
+#            message.append('Edits to {nodes_touched} nodes constitute the '
+#                           'majority of this edit.')
+#        elif len(ways_touched) / total_changes > 0.5:
+#            message.append('Edits to {ways_touched} ways constitute the '
+#                           'majority of this edit.')
+#        elif len(relations_touched) / total_changes > 0.5:
+#            message.append('Edits to {relations_touched} relations constitute',
+#                           'the majority of this edit.')
+
+        return (' '.join(message)).format(**msg_data)
